af7e2f83aafbc432b8cc054b63a888c126e8fdde
Sundus <sundusnoor89@gmail.com>
2025-11-09 23:32:03 +0300

    Added and modified the code

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,10 @@
 const args = process.argv.slice(2);
  if (args.length === 0 )
  {
-    console.log("No arguments");
+    console.log("No argument");
  }
- else {
-    console.log("Arguments found");
+ else if(args.length === 1 ) {
+    console.log("Argument found");
+ } else {
+    console.log("Arguments found")
  }
-
