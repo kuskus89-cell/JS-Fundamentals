8405ad0591e9834671e4b4d3a35ceffb06f06469
Sundus <sundusnoor89@gmail.com>
2025-11-09 21:31:13 +0300

    Added js files with argv command

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+const args = process.argv;
+ if (args.length === 0 )
+ {
+    console.log("No arguments");
+ } else if (args.length === 1)
+ {
+    console.log("Arguments found");
+ } else {
+    console.log("Arguments found");
+ }
+
+
