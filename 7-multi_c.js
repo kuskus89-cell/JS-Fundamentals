367340b6b50812a93af21e30ef3b1c3f9ba2fe36
Sundus <sundusnoor89@gmail.com>
2025-11-12 14:12:29 +0300

    Added another loop

---
+++ 7-multi_c.js
@@ -0,0 +1,8 @@
+const x = process.argv[2];
+if (!isNaN(x)) {
+    for (let i = 0; i < Number(x); i++) {
+        console.log('C is fun');
+    }
+}else {
+    console.log('Missing number of occurrences');   
+}
