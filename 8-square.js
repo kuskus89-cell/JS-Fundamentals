e431c3a47e5ebe4ffe61b0e5ac2c830b9511e09a
Sundus <sundusnoor89@gmail.com>
2025-11-16 23:57:17 +0300

    modified the code

--- 8-square.js
+++ 8-square.js
@@ -1,7 +1,7 @@
 const size = process.argv[2];
-if(!isNaN(size)){
+if(!isNaN(size) && size !== '') {
     const n = Number(size);
-    for(let i=0; i<n; i++){
+    for (let i=0; i<n; i++){
         console.log('x'.repeat(n));
 }
 } else {
